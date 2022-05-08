b87456d7f60f6e0b6296314efd5dce5a386f328b
ArmyAntSEC <55052666+ArmyAntSEC@users.noreply.github.com>
2022-05-08 07:56:24 +0200

    Cleaned up config. Made ID configurable

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int LED = 13;
 volatile int x = 0;
 
 #define master
+#define ID 9
 
 bool ledOn = false;
 
@@ -33,7 +34,13 @@ void setup()
     while (!Serial) {}
 
     Log << "*************************" << endl;
-    Log << "Hello I2C World!" << endl;
+    Log << "Hello I2C World" << endl;
+    #ifdef ID
+        Log << "ID: " << ID << endl;
+    #else
+        Log << "Master" << endl;
+    #endif
+
     pinMode( 13, OUTPUT );
     
     delay(1000);
@@ -41,7 +48,7 @@ void setup()
     #ifdef master
         Wire.begin();    
     #else
-        Wire.begin(8);     
+        Wire.begin(ID);     
         Wire.onRequest(onRequest);
         Wire.onReceive(onReceive);
         
@@ -55,10 +62,10 @@ void loop()
     #ifdef master
         Serial.print ( millis() );
         Serial.print ( "::" );
-        Wire.beginTransmission(8); 
+        Wire.beginTransmission(9); 
         Wire.write("Hello World!");                     
         Wire.endTransmission();                                    
-        int n = Wire.requestFrom(8, 2);
+        int n = Wire.requestFrom(9, 2);
         Serial.print ( n );
         while ( Wire.available() ) {
             char c = Wire.read();
